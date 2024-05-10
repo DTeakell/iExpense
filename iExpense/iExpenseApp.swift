7284a6bba68ec71a53f5b061bb7cf22e14d36bc4
Dillon Teakell <teakelld@icloud.com>
2024-05-10 12:39:31 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dillon Teakell on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
