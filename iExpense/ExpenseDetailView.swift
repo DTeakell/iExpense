87fce00a654eabeb78505afb737776884bbe0755
Dillon Teakell <teakelld@icloud.com>
2024-05-17 14:14:47 -0400

    Update iExpense

---
+++ iExpense/ExpenseDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseDetailView.swift
+//  iExpense
+//
+//  Created by Dillon Teakell on 5/17/24.
+//
+
+import SwiftUI
+
+struct ExpenseDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseDetailView()
+}
